4ee0b969b3a2b8ef9a92080b9fb0d13a70ae7483
juliacazeri <juliaczanatta@gmail.com>
2025-07-15 13:05:51 -0300

    Aula 05: Desafio

---
+++ src/Aula05/Desafio.java
@@ -0,0 +1,26 @@
+package Aula05;
+
+import java.util.Scanner;
+
+public class Desafio {
+
+    public static void main (String[] args){
+        Scanner leitorDeEntradas = new Scanner(System.in);
+
+        float valorSalario = leitorDeEntradas.nextFloat();
+        float valorBeneficios = leitorDeEntradas.nextFloat();
+
+        float valorImposto = 0;
+
+        if(valorSalario >= 0.00 && valorSalario <= 1100.00){
+            valorImposto = 0.05F * valorSalario;
+        } else if(valorSalario >= 1100.01 && valorSalario <= 2500.00) {
+            valorImposto = 0.10F * valorSalario;
+        } else{
+            valorImposto = 0.15F * valorSalario;
+        }
+
+        float saida = valorSalario - valorImposto + valorBeneficios;
+        System.out.println(String.format("%.2f", saida));
+    }
+}
