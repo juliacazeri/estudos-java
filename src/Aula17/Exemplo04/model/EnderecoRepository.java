1afdfddf43ab35f7553c848d7a8ee6844bb4ecfc
juliacazeri <juliaczanatta@gmail.com>
2025-08-04 10:47:42 -0300

    Aula 17: Padrões de projeto em API REST

---
+++ src/Aula17/Exemplo04/model/EnderecoRepository.java
@@ -0,0 +1,13 @@
+package Aula17.Exemplo04.model;
+
+/*
+package juliacazeri.projeto.spring.model;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EnderecoRepository extends CrudRepository<Endereco, String>{
+
+}
+ */
---
+++ src/Aula17/Exemplo04/service/ViaCepService.java
@@ -0,0 +1,17 @@
+package Aula17.Exemplo04.service;
+
+/*
+package juliacazeri.projeto.spring.service;
+
+import juliacazeri.projeto.spring.model.Endereco;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "viacep", url = "https://viacep.com.br/ws")
+public interface ViaCepService{
+
+    @GetMapping("/{cep}/json/")
+    Endereco consultarCep(@PathVariable("cep") String cep);
+}
+ */
